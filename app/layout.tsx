02f124e9357c255554dfc1d20d33bd6e2cb345e7
jhsia314 <jhsia314@gmail.com>
2026-04-10 22:15:50 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,9 @@ export default function RootLayout({
       className={`${satoshi.variable} ${geistMono.variable} h-full antialiased`}
     >
      <body className="min-h-full flex flex-col">
-  {children}
+  <Providers>
+    {children}
+  </Providers>
   <Analytics />
 </body>
     </html>
